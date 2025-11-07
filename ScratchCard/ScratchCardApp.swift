dc4d585c354e5bb5f20ddf9fce505476a4059ca8
Vladimir Calfa <vladimir.calfa@>
2025-11-08 00:41:33 +0100

    Initial Commit

---
+++ ScratchCard/ScratchCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardApp.swift
+//  ScratchCard
+//
+//  Created by Vladimir Calfa on 08/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
